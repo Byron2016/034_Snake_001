deb5e5d640df58043efd1aaf3f7b11ea48ca1c84
Byron2016 <bgva2005@yahoo.com>
2024-02-18 12:32:57 -0500

    b_vite: weibenfalk-react create a function structure and show canvas

--- projects/b_snake_weibenfalk_react/src/App.jsx
+++ projects/b_snake_weibenfalk_react/src/App.jsx
@@ -1,9 +1,72 @@
 import './App.css'
 
+import { useState, useRef, useEffect } from 'react'
+
+// hooks
+import { useInterval } from './hooks/useInterval'
+
+// constants
+import {
+  CANVAS_SIZE,
+  SNAKE_START,
+  APPLE_START,
+  SCALE,
+  SPEED,
+  DIRECTIONS
+} from './constants/constants'
+
 function App () {
+  const canvasRef = useRef(null)
+  const [snake, setSnake] = useState(SNAKE_START)
+  const [apple, setApple] = useState(APPLE_START)
+  const [dir, setDir] = useState([0, -1])
+  const [speed, setSpeed] = useState(null)
+  const [gameOver, setGameOver] = useState(false)
+
+  const startGame = () => {
+    console.log('startGame')
+  }
+
+  const endGame = () => {
+
+  }
+
+  const moveSnake = () => {
+    console.log('moveSnake')
+  }
+
+  const createApple = () => {
+
+  }
+
+  const checkCollision = () => {
+
+  }
+
+  const checkappleCollision = () => {
+
+  }
+
+  const gameLoop = () => {
+
+  }
+
+  useEffect(() => {
+    // It is will triggered when change: snake, apple, gameOver
+  }, [snake, apple, gameOver])
+
   return (
     <>
-      <h1>Hi</h1>
+      <div role='button' tabIndex='0' onKeyDown={e => moveSnake(e)}>
+        <canvas
+          style={{ border: '1px solid black' }}
+          ref={canvasRef}
+          width={`${CANVAS_SIZE[0]}px`}
+          height={`${CANVAS_SIZE[1]}px`}
+        />
+        {gameOver && <div>GAME OVER</div>}
+        <button onClick={startGame}>Start Game</button>
+      </div>
     </>
   )
 }
