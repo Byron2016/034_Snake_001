08a7f49eaad99e331bd0b9113cec8781ecb9ef01
Byron2016 <bgva2005@yahoo.com>
2024-02-19 21:26:23 -0500

    b_vite: w-react-j_aa draw snake body

--- projects/b_snake_weibenfalk_react_jhampier_001/src/App.jsx
+++ projects/b_snake_weibenfalk_react_jhampier_001/src/App.jsx
@@ -23,6 +23,7 @@ import { EyesToDraw } from './components/Snake/drawEyes'
 import { drawApple } from './components/Apple/drawApple'
 
 import { GAME_LOOP_CONSOL, IS_DEVELOPMENT } from './config'
+import { drawBody } from './components/Snake/drawBody'
 
 function App () {
   const canvasRef = useRef(null)
@@ -114,8 +115,15 @@ function App () {
     context.clearRect(0, 0, CANVAS_SIZE[0], CANVAS_SIZE[1])
     drawBackground({color:COLOR_BACKGROUND[0], ctx:context})
 
-    // Head
+    
     context.setTransform(SCALE, 0, 0, SCALE, 0, 0)
+
+    // Snake body 
+    snake.forEach(([x, y]) => {
+      drawBody({position:{x,y}, ctx:context})
+    });
+
+    // Head
     drawHead({position:{x:snake[0][0],y:snake[0][1]}, ctx:context})
 
     // Eyes
@@ -124,8 +132,10 @@ function App () {
       eye_two_position:{x:snake[0][0],y:snake[0][1]},
       ctx:context})
 
-    // Apple
-    drawApple({position:{x:apple[0],y:apple[1]}, ctx:context})
+    // // Apple
+    // drawApple({position:{x:apple[0],y:apple[1]}, ctx:context})
+
+
 
     //context.clearRect(0, 0, CANVAS_SIZE[0], CANVAS_SIZE[1])
     //context.fillStyle = 'pink'
---
+++ projects/b_snake_weibenfalk_react_jhampier_001/src/components/Snake/drawBody.js
@@ -0,0 +1,27 @@
+// Utils
+import drawCircle from "../utils/drawCircle";
+
+// constants
+import { DRAW_BODY } from "../../constants/body_constants";
+
+export function drawBody({
+  position, //headPosition
+  ctx,
+
+}) {
+  const radio = DRAW_BODY.BODY_RADIO;
+  const color = DRAW_BODY.BODY_COLOR;
+  const shadowColor = DRAW_BODY.BODY_SHADOW_COLOR;
+  const transparency = DRAW_BODY.BODY_TRANSPARENCY
+  const shadowBlur = DRAW_BODY.BODY_SHADOW_BLUR;
+  drawCircle({
+    position,
+    radio,
+    color,
+    transparency,
+    shadowColor,
+    shadowBlur,
+    ctx,
+  });
+
+}
